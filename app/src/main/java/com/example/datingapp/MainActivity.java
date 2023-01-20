4dbdd309b843de4918853a60db41a1e1e4b12747
patrikselberg <patrik.selberg@edu.newton.se>
2023-01-20 18:25:22 +0100

    Firebase and splash screen
    Imported my previous work with the firebase setup and implemented the splash screen from the other branch.

--- app/src/main/java/com/example/datingapp/MainActivity.java
+++ app/src/main/java/com/example/datingapp/MainActivity.java
@@ -1,34 +1,53 @@
 package com.example.datingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE); //hide the title
-        getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.activity_main);
 
-        AppCompatButton buttonLogIn = findViewById(R.id.buttonLogIn);
-        AppCompatButton buttonSignUp = findViewById(R.id.buttonSignUp);
-        buttonLogIn.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            setContentView(R.layout.authorized);
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        textView = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+
+        if(user == null){
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        } else {
+            textView.setText(user.getEmail());
         }
-        });
-        buttonSignUp.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                setContentView(R.layout.signup);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+
             }
         });
     }
 }
+
 
---
+++ app/src/main/java/com/example/datingapp/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.datingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },2700);
+    }
+}
