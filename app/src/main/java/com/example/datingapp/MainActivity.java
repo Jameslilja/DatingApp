37a52cac9ea6f95dd96ef3ec86a9430dc1e18ccc
Jameslilja <89815334+Jameslilja@users.noreply.github.com>
2023-01-20 12:23:20 +0100

    Fixed an register page with Muhammad It lags when i press the Sign upp button

--- app/src/main/java/com/example/datingapp/MainActivity.java
+++ app/src/main/java/com/example/datingapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.datingapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,11 +16,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AppCompatButton button = (AppCompatButton) findViewById(R.id.buttonLogIn);
+        AppCompatButton signUp = (AppCompatButton) findViewById(R.id.buttonSignUp);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        }
+
         //button.setOnClickListener(new View.OnClickListener() {
         //public void onClick(View v) {
         // Do something in response to button click
         //}
         //});
     }
-}
 
