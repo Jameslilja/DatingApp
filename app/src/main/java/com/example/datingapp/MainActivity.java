d05b27f46e91d3f20421d911ebe0cf0f8b98fc5e
Jameslilja <89815334+Jameslilja@users.noreply.github.com>
2023-02-10 01:04:45 +0100

    Prepared a Java-code in the other profile activity with Muhammed and fixed the search in the appbar so you can type in the searchbar.

--- app/src/main/java/com/example/datingapp/MainActivity.java
+++ app/src/main/java/com/example/datingapp/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.datingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,12 +34,31 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater= getMenuInflater();
         inflater.inflate(R.menu.menu,menu);
+
+        MenuItem.OnActionExpandListener onActionExpandListener=new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                Toast.makeText(MainActivity.this, "Search is expanded", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                Toast.makeText(MainActivity.this, "Search is Collapsed", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        };
+        menu.findItem(R.id.search).setOnActionExpandListener(onActionExpandListener);
+        SearchView searchView=(SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setQueryHint("Search Data Here...");
         return true;
-    }
-    //button.setOnClickListener(new View.OnClickListener() {
-        //public void onClick(View v) {
-        // Do something in response to button click
-        //}
-        //});
+
     }
 
+    }
+
+//button.setOnClickListener(new View.OnClickListener() {
+//public void onClick(View v) {
+// Do something in response to button click
+//}
+//});
